64445e276c2ae6ea095135597fa30f43b88e3809
afmireski <afmireski@outlook.com>
2021-07-25 09:39:16 -0300

    Finalização implementação Endereço
    - Adição de Imagem à MainScreen;
    - Finalização dos teste para endereço e integração com o banco.

--- LojaPokemon/src/screens/MainScreen.java
+++ LojaPokemon/src/screens/MainScreen.java
@@ -5,6 +5,14 @@
  */
 package screens;
 
+import functions.ConvertFromEnum;
+import functions.ConvertToEnum;
+import functions.FileManager;
+import functions.VerifyPK;
+import helpers.BuildConfirmDialog;
+import helpers.BuildMessageDialog;
+import helpers.ErrorTools;
+import helpers.GenericComponents;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionListener;
@@ -12,18 +20,43 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Arrays;
 import java.util.List;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import tools.CaixaDeFerramentas;
+import tools.CopiarArquivos;
+import tools.DiretorioDaAplicacao;
+import tools.ImagemAjustada;
+import tools.Tools;
 
 /**
  *
  * @author AFMireski
  */
 public class MainScreen extends JFrame {
+    
+    //INSTANCIA DOS HELPERS
+    GenericComponents components = new GenericComponents();
+    BuildMessageDialog messageDialog;
+    BuildConfirmDialog confirmDialog;
+    ErrorTools errorTools = new ErrorTools();
+
+//INSTANCIA DOS FUNCTIONS
+    ConvertFromEnum convertFromEnum = new ConvertFromEnum();
+    ConvertToEnum convertToEnum = new ConvertToEnum();
+    VerifyPK verifyPK = new VerifyPK();
+    FileManager fileManager = new FileManager();
+
+//INSTANCIA DAS TOOLS
+    final private CaixaDeFerramentas cf = new CaixaDeFerramentas();
+    final private ImagemAjustada imagemAjustada = new ImagemAjustada();
+    final private DiretorioDaAplicacao dda = new DiretorioDaAplicacao();
+    final private CopiarArquivos copiarArquivos = new CopiarArquivos();
+    Tools tools = new Tools();
 
     //INSTANCIA DOS CONTAINERS
     Container container;
@@ -35,6 +68,9 @@ public class MainScreen extends JFrame {
     JPanel panEast = new JPanel();
     JPanel panWest = new JPanel();
     JPanel panBody = new JPanel();
+    
+//INSTANCIA DOS LABELS
+    JLabel img = new JLabel();
 
 //INSTANCIA DOS MENUS
     JMenu menu = new JMenu("Cadastros");
@@ -68,6 +104,13 @@ public class MainScreen extends JFrame {
 
         return mi;
     }
+    
+    private void setImage() {
+        String path = dda.getDiretorioDaAplicacao() + "/src/images/loja_pokemon.png";
+        System.out.println(path);
+        ImageIcon icon = imagemAjustada.getImagemAjustada(path, 500, 500);
+        img.setIcon(icon);
+    }
 
     public MainScreen() {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -84,6 +127,10 @@ public class MainScreen extends JFrame {
         container.add(panBody, BorderLayout.CENTER);
 
         panNorth.add(new JLabel("LOJA POKÉMON"));
+        
+        setImage();
+        
+        panBody.add(img);
 
         //MENU CONFIGURATIONS
         createMenu();
