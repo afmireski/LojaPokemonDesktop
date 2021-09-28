50b11c086b1441c40b450eb70df16d341e3c2a33
afmireski <afmireski@outlook.com>
2021-09-28 17:04:10 -0300

    - Finalização da geração do recibo

--- LojaPokemon/src/helpers/Recibo.java
+++ LojaPokemon/src/helpers/Recibo.java
@@ -5,6 +5,7 @@
  */
 package helpers;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
@@ -17,6 +18,7 @@ import com.itextpdf.text.pdf.PdfWriter;
 import daos.DAOPedido;
 import daos.DAOPedidoHasPokemon;
 import daos.DAOPokemon;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.util.List;
 import models.Pedido;
@@ -24,38 +26,44 @@ import models.PedidoHasPokemon;
 import models.Pokemon;
 import models.Preco;
 import tools.CaixaDeFerramentas;
+import tools.DiretorioDaAplicacao;
 
 /**
  *
  * @author AFMireski
  */
 public class Recibo {
+
     private static final String MSG_FALHA_GERAR_RELATORIO = "Houve uma falha ao gerar o relatório da venda do pedido %d";
     private static final String MSG_FALHA_ENVIAR_RELATORIO = "Houve uma falha ao "
             + "enviarmos o relatório da venda do pedido %d para o e-mail %s";
-    
+
     public static void generateReciboByPedido(Integer pedidoID) throws Exception {
         final Font titleFont = FontFactory.getFont(FontFactory.HELVETICA, 16, Font.BOLD, new CMYKColor(89, 34, 0, 64));
         final Font enphasisFont = FontFactory.getFont(FontFactory.HELVETICA, 14, Font.BOLD, new CMYKColor(89, 34, 0, 14));
         final Font defaultFont = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD);
-        final Font tableColHeaderFont = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD, new CMYKColor(89, 34, 0, 14));
+        final Font tableColHeaderFont = FontFactory.getFont(FontFactory.HELVETICA, 12, Font.BOLD, BaseColor.WHITE);
 
         final DAOPedidoHasPokemon daoPHP = new DAOPedidoHasPokemon();
         final DAOPedido daoPedido = new DAOPedido();
         final DAOPokemon daoPokemon = new DAOPokemon();
 
         final CaixaDeFerramentas cf = new CaixaDeFerramentas();
+        final DiretorioDaAplicacao dda = new DiretorioDaAplicacao();
+
+        final String path = String.format(dda.getDiretorioDaAplicacao() + "/src/pdfs/ReciboPedido%s.pdf", pedidoID);
+
+        Document document = new Document();
+
+        PdfWriter writer = PdfWriter.getInstance(
+                document,
+                new FileOutputStream(path));
 
         try {
             ///BUSCA OS DADOS DO PEDIDO
             final List<PedidoHasPokemon> phps = daoPHP.findAllPHPByPedidoID(pedidoID);
             final Pedido pedido = daoPedido.get(pedidoID);
 
-            Document document = new Document();
-
-            PdfWriter writer = PdfWriter.getInstance(
-                    document,
-                    new FileOutputStream(String.format("/pdfs/ReciboPedido%s", pedidoID)));
             document.open();
 
             Paragraph title = new Paragraph("Loja Pokémon", titleFont);
@@ -92,8 +100,8 @@ public class Recibo {
             document.add(br2);
 
             ///CRIA TABELA PARA DETALHAR A COMPOSIÇÂO DO PEDIDO
-            PdfPTable table = new PdfPTable(3);
-            table.setWidthPercentage(100);
+            PdfPTable table = new PdfPTable(2);
+            table.setWidthPercentage(60);
             table.setSpacingBefore(10f);
             table.setSpacingBefore(10f);
 
@@ -114,35 +122,39 @@ public class Recibo {
 
             table.addCell(pokeCol);
             table.addCell(valorCol);
-            
+
             double valor_total = 0.0;
             for (PedidoHasPokemon php : phps) {
                 Pokemon pokemon = daoPokemon.get(php.getPedidoHasPokemonPK().getPokemonID());
                 Preco preco = pokemon.getPrecoList().get(pokemon.getPrecoList().size() - 1);
                 valor_total += preco.getValor();
-                
+
                 PdfPCell pokeNome = new PdfPCell(new Paragraph(pokemon.getNome(), defaultFont));
                 PdfPCell pokeValor = new PdfPCell(new Paragraph(
                         String.format("%.2f", preco.getValor()), defaultFont));
-                
+
                 table.addCell(pokeNome);
                 table.addCell(pokeValor);
             }
-            
+            table.setHorizontalAlignment(Element.ALIGN_LEFT);
             document.add(table);
-            
+
             Paragraph br3 = new Paragraph(" ");
             document.add(br3);
-            
-            
+
             ///MOSTRA O VALOR TOTAL DO PEDIDO
             Paragraph total = new Paragraph(String.format("Total: R$ %.2f", valor_total), titleFont);
             document.add(total);
-            
+
+            document.close();
+            writer.close();
+
+        } catch (Exception e) {
             document.close();
             writer.close();
             
-        } catch (Exception e) {
+            File pdf = new File(path);
+            pdf.delete();
             throw new Exception(String.format(MSG_FALHA_GERAR_RELATORIO, pedidoID));
         }
     }
--- LojaPokemon/src/screens/ReciboScreen.java
+++ LojaPokemon/src/screens/ReciboScreen.java
@@ -60,20 +60,20 @@ public class ReciboScreen extends JDialog {
     JPanel panEast = new JPanel();
     JPanel panWest = new JPanel();
     JPanel panBody = new JPanel();
-    
+
     //BODY PANELS
     JPanel panL1C1 = new JPanel(); //Painel referente a posição da grade: Linha 1 - Coluna 1
     JPanel panL1C2 = new JPanel(); //Painel referente a posição da grade: Linha 1 - Coluna 2
     JPanel panL2C1 = new JPanel(); //Painel referente a posição da grade: Linha 2 - Coluna 1
     JPanel panL2C2 = new JPanel(); //Painel referente a posição da grade: Linha 2 - Coluna 2
-    
+
     //INSTANCIA DOS BUTTONS
     JButton btnGerarRecibo = components.buttonWithIcon("Gerar Recibo", "/icons/receipt.png");
-    
+
     //INSTANCIA DOS LABELS
     JLabel lblRemetente = new JLabel("E-MAIL REMETENTE");
     JLabel lblSenhaRemetente = new JLabel("SENHA E-MAIL");
-    
+
     //INSTANCIA DOS TEXTFIELD
     JTextField txtRemetente = new JTextField(20);
     JPasswordField txtSenha = new JPasswordField(10);
@@ -82,7 +82,7 @@ public class ReciboScreen extends JDialog {
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
         setTitle("GERAR RECIBO");
-        
+
         //CONTAINER CONFIGURATIONS
         container = getContentPane();
         container.setLayout(new BorderLayout());
@@ -91,10 +91,10 @@ public class ReciboScreen extends JDialog {
         container.add(panEast, BorderLayout.EAST);
         container.add(panWest, BorderLayout.WEST);
         container.add(panBody, BorderLayout.CENTER);
-                
+
         panNorth.add(new JLabel("LOJA POKÉMON"));
         panSouth.add(btnGerarRecibo);
-        
+
         //PAN BODY CONFIGURATIONS
         panBody.setBorder(BorderFactory.createLineBorder(Color.black, 5));
         panBody.setLayout(new GridLayout(2, 2));
@@ -104,35 +104,41 @@ public class ReciboScreen extends JDialog {
         panBody.add(panL1C2);
         panBody.add(panL2C1);
         panBody.add(panL2C2);
-        
+
         //Prenchimento Linha 1        
         panL1C1.add(lblRemetente);
         panL1C2.add(txtRemetente);
 
         //Prenchimento Linha 2        
         panL2C1.add(lblSenhaRemetente);
-        panL2C2.add(txtSenha);      
-        
+        panL2C2.add(txtSenha);
+
         btnGerarRecibo.addActionListener((ae) -> {
             try {
-                System.out.println(txtSenha.getPassword().toString());
-                
-                Recibo.generateReciboByPedido(pedidoID);
-            } catch (Exception e) {
-                messageDialog = new BuildMessageDialog(
-                            DialogMessageType.ERROR,
-                            e.getMessage(),
-                            "RECEIPT Error",
+                if (txtRemetente.getText().isEmpty() || txtSenha.getPassword().length == 0) {
+                    throw new Exception("Preencha os campos corretamente!!!");
+                } else {
+                    Recibo.generateReciboByPedido(pedidoID);
+                    messageDialog = new BuildMessageDialog(
+                            DialogMessageType.SUCESS,
+                            "Recibo gerado com sucesso",
+                            "RECEIPT Generated",
                             container);
+                }
+            } catch (Exception e) {
+                errorTools.showExceptionStackTrace(e);
+                messageDialog = new BuildMessageDialog(
+                        DialogMessageType.ERROR,
+                        e.getMessage(),
+                        "RECEIPT Error",
+                        container);
             }
-            
+
         });
-        
+
         pack();
         setModal(true);
         setVisible(true);
     }
-    
-    
 
 }
